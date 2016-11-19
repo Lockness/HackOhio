84afb7bc1aeaf0bfbf2b6d3f2956470f0048c15c
ryantomlinson95 <ryantomlinson95@gmail.com>
2016-11-19 17:46:47 -0500

    google search api calls added

--- AccessibilityApp/config/routes.rb
+++ AccessibilityApp/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   
+  get 'results/listings'
+
   resources :locations
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
