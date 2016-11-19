41e981f7344bfd6a6adf8fa4ae338ad8a955b7c7
ryantomlinson95 <ryantomlinson95@gmail.com>
2016-11-19 12:46:02 -0500

    whatever

--- AccessibilityApp/config/routes.rb
+++ AccessibilityApp/config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  Rails.application.routes.draw do
+    get "/pages/:page" => "pages#show"
+  end
   # You can have the root of your site routed with "root"
-   root 'home#index'
-
+  #root 'home#index'
+  root "pages#show", page: "home"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
