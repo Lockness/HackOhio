5d508083a0b266e1e45d596afa5e8864d0b52d2d
Jtcruthers <jtcruthers@gmail.com>
2016-11-19 20:07:02 -0500

    Added listings and refactored code

--- AccessibilityApp/app/controllers/pages_controller.rb
+++ AccessibilityApp/app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
 
-  	def show
+    def show
     	render template: "pages/#{params[:page]}"
     end 
+
+    def testt
+        location = Location.new
+        location.save
+    end
+
+    helper_method :testt
 end
