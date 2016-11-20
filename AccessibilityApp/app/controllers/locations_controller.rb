aeef649d7ca66e1e8390fd1df8ebf20b25bc7088
Jtcruthers <jtcruthers@gmail.com>
2016-11-20 04:21:36 -0500

    Added links to locations

--- AccessibilityApp/app/controllers/locations_controller.rb
+++ AccessibilityApp/app/controllers/locations_controller.rb
@@ -64,7 +64,7 @@ class LocationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_location
-      @location = Location.find(params[:id])
+      @location = Location.find_by placeid: params[:placeid]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
